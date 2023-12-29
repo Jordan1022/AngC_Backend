14f13210447e74ed0dd36a7672ae7132275ad6a8
Jordan Allen <land.of.jordan@gmail.com>
2023-12-28 18:22:23 -0600

    env and more

--- database.js
+++ database.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2/promise');
 
 async function initializeDatabase() {
     const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'your_username',
-        password: 'your_password',
-        database: 'your_database'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     });
 
     // Create tables if needed
